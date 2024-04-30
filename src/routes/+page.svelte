b7f3db68a2a276d02c787af55fb19963d711b036
Aydin Elhan <aydinmate@protonmail.com>
2024-04-30 20:29:42 +1000

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col items-center p-[5rem] justify-between h-[100vh]">
+    <div>
+        <p>Text</p>
+    </div>
+    <div class="flex items-center space-x-2 w-[40%]">
+        <input type="text" placeholder="Type here" class="input input-bordered w-full" />
+        <button class="btn btn-active btn-accent">Send</button>
+    </div>
+</div>
