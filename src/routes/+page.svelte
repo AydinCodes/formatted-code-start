0806f3175b7cabd1903e840ff419cc55bd9a70d9
Aydin Elhan <aydinmate@protonmail.com>
2024-04-30 20:55:55 +1000

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,45 @@
+<script lang="ts">
+	import OpenAI from 'openai';
+	import { PUBLIC_OPENAI_API_KEY } from '$env/static/public';
+
+	let message: string = '';
+	let result: string | null = null;
+	let loading: boolean = false;
+
+	const openai = new OpenAI({ apiKey: PUBLIC_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
+
+	async function getResult() {
+		loading = true;
+		const completion = await openai.chat.completions.create({
+			messages: [
+				{ role: 'system', content: 'You are a helpful assistant.' },
+				{ role: 'user', content: message }
+			],
+			model: 'gpt-3.5-turbo'
+		});
+
+		result = completion.choices[0].message.content;
+		loading = false;
+		message = '';
+	}
+</script>
+
 <div class="flex flex-col items-center p-[5rem] justify-between h-[100vh]">
-    <div>
-        <p>Text</p>
-    </div>
-    <div class="flex items-center space-x-2 w-[40%]">
-        <input type="text" placeholder="Type here" class="input input-bordered w-full" />
-        <button class="btn btn-active btn-accent">Send</button>
-    </div>
+	<div class="px-[2rem] w-[40%]">
+		{#if loading}
+			<p>Loading...</p>
+		{/if}
+		{#if result}
+			<p>{result}</p>
+		{/if}
+	</div>
+	<div class="flex items-center space-x-2 w-[40%]">
+		<input
+			bind:value={message}
+			type="text"
+			placeholder="Type here"
+			class="input input-bordered w-full"
+		/>
+		<button on:click={getResult} class="btn btn-active btn-accent">Send</button>
+	</div>
 </div>
